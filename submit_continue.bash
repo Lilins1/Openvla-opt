75c4bd71e0cca68e2c151aa391271d757636ae24
Lilins1 <1872156233@qq.com>
2025-06-27 02:17:28 +0200

    action train continue eval

--- submit_continue.bash
+++ submit_continue.bash
@@ -49,15 +49,15 @@ torchrun --standalone --nnodes 1 --nproc-per-node 1 vla-scripts/finetune_RNN.py 
   --data_root_dir /mimer/NOBACKUP/groups/naiss2024-5-164/Ruizhe/OPENVLA/modified_libero_rlds \
   --dataset_name libero_spatial_no_noops \
   --run_root_dir /mimer/NOBACKUP/groups/naiss2024-5-164/Ruizhe/OPENVLA/rnn_model/Bezier/libero/lora_train \
-  --load_Lora_path /mimer/NOBACKUP/groups/naiss2024-5-164/Ruizhe/OPENVLA/rnn_model/Bezier/libero/lora_train/openvla-7b+libero_spatial_no_noops+b16+lr-0.0005+lora-r128+dropout-0.0--image_aug--Bezier1Curve_libero_acc16--20000_chkpt \
+  --load_Lora_path /mimer/NOBACKUP/groups/naiss2024-5-164/Ruizhe/OPENVLA/rnn_model/action/libero/lora_train/openvla-7b+libero_spatial_no_noops+b16+lr-0.0005+lora-r128+dropout-0.0--image_aug--action8chunk_libero_acc16--80000_chkpt \
   --use_film False \
   --num_images_in_input 2 \
   --use_proprio True \
   --batch_size 8 \
   --grad_accumulation_steps 2\
   --learning_rate 5e-4 \
-  --num_steps_before_decay 50005 \
-  --max_steps 120005 \
+  --num_steps_before_decay 30005 \
+  --max_steps 80005 \
   --save_freq 10000 \
   --save_latest_checkpoint_only False \
   --image_aug True \
@@ -69,4 +69,4 @@ torchrun --standalone --nnodes 1 --nproc-per-node 1 vla-scripts/finetune_RNN.py 
   --finetune_lora True\
   --save_vla True\
   --rnn_in_batch True\
-  --run_id_note Bezier1Curve_libero_acc16
+  --run_id_note action8chunk_libero_acc16
